d92187be200d6a4fdb5762a9035fbd8f6f0ae348
Edyluisrey <eaguirre@innovationssystems.com>
2013-06-17 11:35:08 -0500

    d

--- config/application.config.php
+++ config/application.config.php
@@ -4,7 +4,8 @@ return array(
     'modules' => array(
         'Application',
         'Usuario',
-        'Admin'
+        'Admin',
+    
     ),
 
     // These are various options for the listeners attached to the ModuleManager
