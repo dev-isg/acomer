81a6a5695e0341b852c1c818814df5fbb1c00114
kevcast15@gmail.com <kevcast15@gmail.com>
2013-06-17 11:32:16 -0500

    cambios_01

--- config/application.config.php
+++ config/application.config.php
@@ -4,7 +4,8 @@ return array(
     'modules' => array(
         'Application',
         'Usuario',
-        'Admin'
+        'Admin',
+        'Restaurant',
     ),
 
     // These are various options for the listeners attached to the ModuleManager
