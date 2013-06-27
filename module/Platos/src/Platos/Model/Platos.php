f818cbd449c6da2f6650535fb512df982d0bace3
kevcast15@gmail.com <kevcast15@gmail.com>
2013-06-27 09:35:23 -0500

    cambios_01

--- module/Platos/src/Platos/Model/Platos.php
+++ module/Platos/src/Platos/Model/Platos.php
@@ -24,6 +24,7 @@ class Platos implements InputFilterAwareInterface
     public $Ta_puntaje_in_id;
     public $Ta_usuario_in_id;
     public $tipo_plato_va_nombre;
+    public $restaurante_va_nombre;
 //    
 //    private $data=array();
 //    
@@ -42,17 +43,17 @@ class Platos implements InputFilterAwareInterface
     
     public function exchangeArray($data)
     {
-//        $this->in_id = (!empty($data['in_id'])) ? $data['in_id'] : null;
-//        $this->va_imagen = (!empty($data['va_imagen'])) ? $data['va_imagen'] : null;
-//        $this->tx_descripcion = (!empty($data['tx_descripcion'])) ? $data['tx_descripcion'] : null;
-//        $this->va_nombre = (!empty($data['va_nombre'])) ? $data['va_nombre'] : null;
-//        $this->va_precio = (!empty($data['va_precio'])) ? $data['va_precio'] : null;
-//        $this->en_destaque = (!empty($data['en_destaque'])) ? $data['en_destaque'] : null;
-//        $this->en_estado = (!empty($data['en_estado'])) ? $data['en_estado'] : null;
-//        $this->Ta_tipo_plato_in_id = (!empty($data['Ta_tipo_plato_in_id'])) ? $data['Ta_tipo_plato_in_id'] : null;
-//        $this->Ta_puntaje_in_id = (!empty($data['Ta_puntaje_in_id'])) ? $data['Ta_puntaje_in_id'] : null;
-//        $this->Ta_usuario_in_id = (!empty($data['Ta_usuario_in_id'])) ? $data['Ta_usuario_in_id'] : null;
-//        
+        $this->in_id = (!empty($data['in_id'])) ? $data['in_id'] : null;
+        $this->va_imagen = (!empty($data['va_imagen'])) ? $data['va_imagen'] : null;
+        $this->tx_descripcion = (!empty($data['tx_descripcion'])) ? $data['tx_descripcion'] : null;
+        $this->va_nombre = (!empty($data['va_nombre'])) ? $data['va_nombre'] : null;
+        $this->va_precio = (!empty($data['va_precio'])) ? $data['va_precio'] : null;
+        $this->en_destaque = (!empty($data['en_destaque'])) ? $data['en_destaque'] : null;
+        $this->en_estado = (!empty($data['en_estado'])) ? $data['en_estado'] : null;
+        $this->Ta_tipo_plato_in_id = (!empty($data['Ta_tipo_plato_in_id'])) ? $data['Ta_tipo_plato_in_id'] : null;
+        $this->Ta_puntaje_in_id = (!empty($data['Ta_puntaje_in_id'])) ? $data['Ta_puntaje_in_id'] : null;
+        $this->Ta_usuario_in_id = (!empty($data['Ta_usuario_in_id'])) ? $data['Ta_usuario_in_id'] : null;
+        
 //        $this->hola = (!empty($data['hola'])) ? $data['hola'] : null;
     }
 // Add content to these methods:
