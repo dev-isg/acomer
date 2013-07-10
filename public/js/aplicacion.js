c3549066ccc0a2230eeeacd9fe4770069775bde0
joseluis <jyana@innovationssystems.com>
2013-07-10 16:07:48 -0500

    js

--- public/js/aplicacion.js
+++ public/js/aplicacion.js
@@ -160,45 +160,6 @@ $(document).ready(function(){
                 $(".agregar-comentario-desc").slideUp();
             }
         });
-//comentarios validacion
-
-var validar=function(elemento){
-   $(elemento).validate({
-        rules: {
-            va_nombre: {
-                required: true
-            },
-            va_email: {
-                required: true,
-                email : true           
-            },
-            tx_descripcion:{
-                required : true                       
-            }            
-        },
-        messages:{
-            va_nombre: {
-                required:"Por favor ingresar el nombre del plato"
-            },
-            tx_descripcion:{
-                required:"Por favor ingresar una comentario"
-            },                
-            va_email :{
-                required : "Por favor ingresar un email"                
-            }
-        },
-        highlight: function(element) {
-            $(element).closest('.control-group').removeClass('success').addClass('error');
-        },
-        success: function(element) {
-            element
-            .addClass('valid')
-            .closest('.control-group').removeClass('error').addClass('success');
-        }
-        });
-}
-validar('#comentarios');
-       
 
             $('.cover').mosaic({
                    animation    :    'slide',
