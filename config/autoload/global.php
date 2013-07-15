a8c1a0bad21311af0e41068b215b18d54fee8fd7
Juan Carbajal <jcarbajal@innovationssystems>
2013-07-15 11:01:03 -0500

    baseUrl  ->host('base')

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -10,68 +10,67 @@
  * control, so do not include passwords or other sensitive information in this
  * file.
  */
-
-
 return array(
     // ...AGREGASTE PARA LA CONEXION GLOBAL
-        'db' => array(
-        'driver'         => 'Pdo',
-
-            'username' => 'kevin',
+    'db' => array(
+        'driver' => 'Pdo',
+        
+        'username' => 'kevin',
         'password' => '123456',
-        'dsn'            => 'mysql:dbname=bd_acomer;host=192.168.1.40',
-
+        'dsn' => 'mysql:dbname=bd_acomer;host=192.168.1.40',
+        
         'driver_options' => array(
             PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
-        ),
+        )
     ),
-     'solr' => array (
-                               'host' => '192.168.1.38',
-                               'port' => '8983',
-                               'folder' => '/solr' 
-               ),
-               'host' => array (
-                               'static' => 'http://192.168.1.38:8080',
-                               'images' => 'http://192.168.1.38:8080/imagenes'
-               ),
-               'upload' => array(
-                       'images' => APPLICATION_PATH . '/public/imagenes'
-               ),
+    'solr' => array(
+        'host' => '192.168.1.38',
+        'port' => '8983',
+        'folder' => '/solr'
+    ),
+    'host' => array(
+        'base' => '',
+        'static' => 'http://192.168.1.38:8080',
+        'images' => 'http://192.168.1.38:8080/imagenes'
+    ),
+    'upload' => array(
+        'images' => APPLICATION_PATH . '/public/imagenes'
+    ),
     'service_manager' => array(
         'factories' => array(
-            'Zend\Db\Adapter\Adapter'
-                    => 'Zend\Db\Adapter\AdapterServiceFactory',
-        ),
+            'Zend\Db\Adapter\Adapter' => 'Zend\Db\Adapter\AdapterServiceFactory'
+        )
     ),
     'module_layouts' => array(
-//      'Application' =>  array('layout/layout-portada'),
+        // 'Application' => array('layout/layout-portada'),
         'Application' => array(
-          'index' => 'layout/layout-portada',
-          'terminos'=>'layout/layout-portada',
-          'nosotros'=>'layout/layout-portada',
-          'solicita'=>'layout/layout-portada',
-         'ver'=>'layout/layout-portada',
-            'detalleubicacion'=>'layout/layout-portada',
+            'index' => 'layout/layout-portada',
+            'terminos' => 'layout/layout-portada',
+            'nosotros' => 'layout/layout-portada',
+            'solicita' => 'layout/layout-portada',
+            'ver' => 'layout/layout-portada',
+            'detalleubicacion' => 'layout/layout-portada'
         ),
-      'Local' => array(
-          'index' => 'layout/layout-administrador',
+        'Local' => array(
+            'index' => 'layout/layout-administrador'
+        )
+        ,
+        'Platos' => array(
+            'index' => 'layout/layout-administrador',
+            'verplatos' => 'layout/layout-portada'
+        ),
+        'Usuario' => array(
+            'index' => 'layout/layout-administrador',
+            'comentarios' => 'layout/layout-administrador'
+        )
+        ,
+        'Restaurante' => array(
+            'index' => 'layout/layout-administrador'
+        // 'comentarios'=>'layout/layout-administrador'
+                )
 
-        ),
-      'Platos' => array(
-          'index' => 'layout/layout-administrador',
-          'verplatos'=>'layout/layout-portada'
-        ),
-      'Usuario' => array(
-          'index' => 'layout/layout-administrador',
-          'comentarios'=>'layout/layout-administrador'
-          
-        ),
-       'Restaurante' => array(
-          'index' => 'layout/layout-administrador',
-//          'comentarios'=>'layout/layout-administrador'
-          
-        ),
-
-     ),
-
-);
+        
+    )
+    
+)
+;
