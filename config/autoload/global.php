a10fe9d00299014e87f73365c81e8b9ec8f0aa0d
Josmel Yupanqui <yyupanqui@innovationssystems.com>
2013-08-05 18:46:49 -0500

    solarborrar

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -20,17 +20,17 @@ return array(
         )
     ),
     'solr' => array(
-        'host' => '192.168.1.40',
+        'host' => '192.168.1.45',
         'port' => '8983',
         'folder' => '/solr'
     ),
    
     'host' => array(
-        'base' => 'http://192.168.1.40:8080',
-        'static' => 'http://192.168.1.40:8080',
-        'images' => 'http://192.168.1.40:8080/imagenes',
-        'img'=>'http://192.168.1.40:8080/img',
-        'ruta' => 'http://192.168.1.40:8080',
+        'base' => 'http://192.168.1.45:8080',
+        'static' => 'http://192.168.1.45:8080',
+        'images' => 'http://192.168.1.45:8080/imagenes',
+        'img'=>'http://192.168.1.45:8080/img',
+        'ruta' => 'http://192.168.1.45:8080',
         'version'=>1,
     ),
 
