f249933b0e989be0a52c33f47f6dc886edb56fb0
Josmel Yupanqui <yyupanqui@innovationssystems.com>
2013-09-03 17:26:37 -0500

    cambios

--- module/Platos/src/Platos/Controller/IndexController.php
+++ module/Platos/src/Platos/Controller/IndexController.php
@@ -637,6 +637,7 @@ class IndexController extends AbstractActionController {
               
           $listarecomendacion = $this->getPlatosTable()->getPlatoxRestaurant($id)->toArray();  
           $texto = 'restaurante:"'.$listarecomendacion[0]['restaurant_nombre'].'"'; 
+          $idplatito = $listarecomendacion[0]['in_id'];
        // var_dump($listarecomendacion[0]['tipo_comida']);exit;
                 $limit = 3;
                 $palabraBuscar = isset($texto) ? $texto : false;
@@ -664,7 +665,7 @@ class IndexController extends AbstractActionController {
                             $query = "($palabraBuscar)";
                             $fq = array(
                                 'sort' => 'random_' . uniqid() . ' asc',
-                                'fq' => 'en_estado:activo AND restaurant_estado:activo  AND distrito:' . $distrito,
+                                'fq' => 'en_estado:activo AND restaurant_estado:activo  AND distrito:' . $distrito.' AND -id:' . $idplatito,
                                 'wt' => 'json' );
                             $resulta = false;
                             if ($query) {
@@ -681,7 +682,7 @@ class IndexController extends AbstractActionController {
                             $query = "($palabraBuscar)";
                             $fq = array(
                                 'sort' => 'random_' . uniqid() . ' asc',
-                                'fq' => 'en_estado:activo AND restaurant_estado:activo',
+                                'fq' => 'en_estado:activo AND restaurant_estado:activo AND -id:' . $idplatito,
                                 'wt' => 'json');
                             $resultados = false;
                             if ($query) {
@@ -699,7 +700,7 @@ class IndexController extends AbstractActionController {
                             $query = "($palabraBuscar)";
                             $fq = array(
                                 'sort' => 'random_' . uniqid() . ' asc',
-                                'fq' => 'en_estado:activo AND restaurant_estado:activo',
+                                'fq' => 'en_estado:activo AND restaurant_estado:activo AND -id:' . $idplatito,
                                 'wt' => 'json');
                             $resulta = false;
                             if ($query) {
@@ -718,10 +719,10 @@ class IndexController extends AbstractActionController {
                             $query = "($palabraBuscar)";
                             $fq = array(
                                 'sort' => 'random_' . uniqid() . ' asc',
-                                'fq' => 'en_estado:activo AND restaurant_estado:activo',
+                                'fq' => 'en_estado:activo AND restaurant_estado:activo AND -id:' . $idplatito,
                                 'wt' => 'json');
                             $resultados = false;
-                            if ($query) {
+                            if ($query){
                                 $solr = \Classes\Solr::getInstance()->getSolr();
                                 if (get_magic_quotes_gpc() == 1) {
                                     $query = stripslashes($query);}
@@ -740,7 +741,7 @@ class IndexController extends AbstractActionController {
                 $query = "($palabraBuscar)";
                 $fq = array(
                     'sort' => 'random_' . uniqid() . ' asc',
-                    'fq' => 'en_estado:activo AND restaurant_estado:activo',
+                    'fq' => 'en_estado:activo AND restaurant_estado:activo AND -id:' . $idplatito,
                     'wt' => 'json' );
                 $resultados = false;
                 if ($query) {
