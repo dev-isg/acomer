26d2c88c02732665474409dcf7d23fd779003d49
Josmel Yupanqui <yyupanqui@innovationssystems.com>
2013-10-23 14:07:35 -0500

    cambios

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -12,8 +12,8 @@ return array(
     // ...AGREGASTE PARA LA CONEXION GLOBAL
     'db' => array(
         'driver' => 'Pdo',
-        'username' => 'root',
-        'password' => '',
+        'username' => 'listasabor',
+        'password' => '123456',
         'dsn' => 'mysql:dbname=listadelsabor3;host=192.168.1.36',
         'driver_options' => array(
             PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
