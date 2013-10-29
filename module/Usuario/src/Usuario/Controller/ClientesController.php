333828b694089335d0d874d1e61ae56f012aa95a
Josmel Yupanqui <yyupanqui@innovationssystems.com>
2013-10-29 11:18:16 -0500

    cambios

--- module/Usuario/src/Usuario/Controller/ClientesController.php
+++ module/Usuario/src/Usuario/Controller/ClientesController.php
@@ -397,7 +397,7 @@ public function getAuthService() {
                              } 
                       else {
                        $loginUrl = $facebook->getLoginUrl(array('scope'=>'email,publish_stream,read_friendlists',  
-                    'redirect_uri'=>$this->_options->host->ruta.'/'
+                   // 'redirect_uri'=>$this->_options->host->ruta.'/'
                            ));   
 
                        }   
