4ca127c4a4c2d9a4c286efe40b1a389ef0ae1bbf
Josmel Yupanqui <yyupanqui@innovationssystems.com>
2013-11-04 14:30:03 -0500

    cambios

--- module/LoginFace/src/LoginFace/Controller/FacebookController.php
+++ module/LoginFace/src/LoginFace/Controller/FacebookController.php
@@ -83,8 +83,8 @@ class FacebookController extends AbstractActionController {
                                         )));
                        
                     }
-                    var_dump($storage->read());exit;
-                //   return $result; 
+                  //  var_dump($storage->read());exit;
+                 return $storage; 
          }
   
     
