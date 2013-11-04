be70bd6337635491f5e8abc5bce192c8901d9838
Josmel Yupanqui <yyupanqui@innovationssystems.com>
2013-11-04 15:09:21 -0500

    cambios

--- module/SanAuth2/src/SanAuth2/Controller/AuthController.php
+++ module/SanAuth2/src/SanAuth2/Controller/AuthController.php
@@ -85,6 +85,7 @@ class AuthController extends AbstractActionController {
                                         )));
                       
                     }
+                    
      // return $this->redirect()->toUrl('/');
     }
     
